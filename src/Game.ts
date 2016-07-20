b2c3d765997269548d1c13825687bbc1d16398bb
Austin Simmons <autto91@gmail.com>
2016-07-19 20:34:46 -0700

    added movement and mouse rotation

--- src/Game.ts
+++ src/Game.ts
@@ -1,6 +1,12 @@
 class SpaceShooter
 {
    game:Phaser.Game;
+   player:Phaser.Sprite; 
+   upKey:Phaser.Key; 
+   downKey:Phaser.Key; 
+   leftKey:Phaser.Key;
+   rightKey:Phaser.Key; 
+   speed:number;
 
    constructor() {
        this.game = new Phaser.Game(1024, 768, Phaser.AUTO, '', {
@@ -10,13 +16,33 @@ class SpaceShooter
 
    preload() {
        this.game.load.image('playerShip', 'assets/PlayerShip.png');
+       this.game.world.setBounds(0, 0, 1024, 768);
+       this.speed = 5;
+       this.upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+       this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+       this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+       this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
    }
 
    create() {
-       var player = this.game.add.sprite(500, 500, 'playerShip');
+       this.player = this.game.add.sprite(500, 500, 'playerShip');
+       this.player.anchor.setTo(0.5, 0.5);
    }
 
    update() {
+       this.player.rotation = this.game.physics.arcade.angleToPointer(this.player);
+       if (this.upKey.isDown) {
+           this.player.y -= this.speed;
+       }
+       if (this.downKey.isDown) {
+           this.player.y += this.speed;
+       }
+       if (this.leftKey.isDown) {
+           this.player.x -= this.speed;
+       }
+       if (this.rightKey.isDown) {
+           this.player.x += this.speed;
+       }
    }
 }
 
